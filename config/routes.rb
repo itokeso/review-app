01f8276a2c962a412979bbc3b784bdac06c3cd41
itokeso <61177741+itokeso@users.noreply.github.com>
2020-05-15 23:19:09 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users　
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+    }
+  
+  devise_scope :user do 
+    post 'users/sign_up/complete' => 'users/registrations#complete' 
+  end
   root "posts#index"
 end
